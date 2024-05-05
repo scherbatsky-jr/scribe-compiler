3329daf15ddca14585a51ac62c53c2cdf5b797ba
Sunil Prajapati <sunilpzpt@gmail.com>
2024-05-05 21:05:48 +0700

    feature: flask gui

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 from s_lexer import lexer
 from s_parser import parser
 from s_intrepreter import Interpreter
+from flask import Flask, render_template, request
+from io import StringIO
+import sys
 
-# def main():
-#     lexer.input("print 3")
-#     for tok in lexer:
-#         print(tok)
-#     print(parser.parse("print 3"))
+app = Flask(__name__)
 
-def main():
-    while True:
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        code = request.form['code']
+        processed_code = code.replace('\n', ' ').replace('\r', ' ')
+
+        output = StringIO()           # Create a StringIO object to capture output
+        sys.stdout = output           # Redirect stdout to the StringIO object
+
         try:
-            s = input('calc > ')
-        except EOFError:
-            break
-        if not s: continue
-        ast = parser.parse(s, lexer=lexer)
-        interpreter = Interpreter()
-        interpreter.visit(ast)
+            # Parsing and interpreting the code
+            ast = parser.parse(processed_code, lexer=lexer)
+            interpreter = Interpreter()
+            interpreter.visit(ast)
+        except Exception as e:
+            output.write(str(e))      # In case of errors in parsing or execution
+
+        # Reset stdout to its original state
+        sys.stdout = sys.__stdout__
+        
+        # Get the string from StringIO object
+        output_str = output.getvalue()
+        return render_template('index.html', code=code, output=output_str)
+    return render_template('index.html', code='', output='')
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
+
